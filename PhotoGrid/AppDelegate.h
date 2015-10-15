73e01be23eedec235353a22a450b9511836ceef9
liuyanhong <liuyh@lesdo.cn>
2015-10-15 10:09:53 +0800

    Initial Commit

---
+++ PhotoGrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoGrid
+//
+//  Created by Hong on 15/10/15.
+//  Copyright © 2015年 Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoGrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoGrid
+//
+//  Created by Hong on 15/10/15.
+//  Copyright © 2015年 Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
